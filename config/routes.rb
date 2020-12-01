9cf7a5845794438ff233d33932bcc815d8910099
meklit <meklit_teshome@diveintocode.jp>
2020-12-01 16:14:57 +0900

    routes edited

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#new'
+  resources :posts do
+    collection do
+      post :confirm
+    end
+  end
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users do
+    member do
+      get :favourite
+    end
+  end
+  resources :favorites, only: [:create, :destroy]
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
